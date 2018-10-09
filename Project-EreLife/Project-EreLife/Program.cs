9330b890387f087fcff1b46181bb7d53c7cab1d0
JustHusk <Justhusk101@gmail.com>
2018-10-09 15:21:06 +0100

    Fucked it

---
+++ Project-EreLife/Project-EreLife/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_EreLife
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("   ▄███████▄    ▄████████  ▄██████▄       ▄█    ▄████████  ▄████████     ███     ");
+            Console.WriteLine("  ███    ███   ███    ███ ███    ███     ███   ███    ███ ███    ███ ▀█████████▄ ");
+            Console.WriteLine("  ███    ███   ███    ███ ███    ███     ███   ███    █▀  ███    █▀     ▀███▀▀██ ");
+            Console.WriteLine("  ███    ███  ▄███▄▄▄▄██▀ ███    ███     ███  ▄███▄▄▄     ███            ███   ▀ ");
+            Console.WriteLine("▀█████████▀  ▀▀███▀▀▀▀▀   ███    ███     ███ ▀▀███▀▀▀     ███            ███     ");
+            Console.WriteLine("  ███        ▀███████████ ███    ███     ███   ███    █▄  ███    █▄      ███     ");
+            Console.WriteLine("  ███          ███    ███ ███    ███     ███   ███    ███ ███    ███     ███     ");
+            Console.WriteLine(" ▄████▀        ███    ███  ▀██████▀  █▄ ▄███   ██████████ ████████▀     ▄████▀   ");
+            Console.WriteLine("               ███    ███            ▀▀▀▀▀▀                                      ");
+            Console.WriteLine("");             ");
+            Console.ReadKey();
+        }
+    }
+}
